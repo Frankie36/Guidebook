71465a2cd09b886c7a6285039f72f7fae402382a
Frankie36 <ngunjiri.k.francis@gmail.com>
2019-10-02 20:02:23 +0300

    Added app screenshots

--- app/src/test/java/com/mystique/guidebook/MainActivityTest.java
+++ app/src/test/java/com/mystique/guidebook/MainActivityTest.java
@@ -1,8 +1,12 @@
 package com.mystique.guidebook;
 
+import android.content.Context;
 import android.os.Build;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.mystique.guidebook.io.ApiInterface;
 import com.mystique.guidebook.model.GuideResponse;
 
@@ -16,41 +20,48 @@ import org.robolectric.annotation.Config;
 
 import java.io.IOException;
 
-import javax.inject.Inject;
-
+import okhttp3.OkHttpClient;
 import okhttp3.ResponseBody;
 import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
 import okhttp3.mockwebserver.RecordedRequest;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
-import static androidx.test.InstrumentationRegistry.getContext;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
 import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertSame;
 import static junit.framework.Assert.assertTrue;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(sdk = Build.VERSION_CODES.O_MR1)
 public class MainActivityTest {
 
-    @Inject
-    private ApiInterface apiInterface;
+    Context context = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
     @Rule
     public MockWebServer mockBackend = new MockWebServer();
 
     @Before
     public void setUp() {
-        configureDi();
+        mockApi();
     }
 
-    private void configureDi() {
-
+    public Retrofit provideRetrofit(String url) {
+        return new Retrofit.Builder()
+                .baseUrl(url)
+                //Custom converters should come before Gson converter
+                .addConverterFactory(GsonConverterFactory.create((new GsonBuilder()
+                        .setLenient()
+                        .create())))
+                .client(new OkHttpClient())
+                .build();
     }
 
     private ApiInterface mockApi() {
-        return MainApplication.getInstance().provideRetrofit(mockBackend.url("/").toString()).create(ApiInterface.class);
+        return provideRetrofit(mockBackend.url("/").toString()).create(ApiInterface.class);
     }
 
     private String fileName = "response_ok";
@@ -58,7 +69,8 @@ public class MainActivityTest {
     //Test to show our builder is configured correctly
     @Test
     public void getsBuilderInstance() {
-        assertNotNull(apiInterface);
+        final ApiInterface messagesApi = provideRetrofit(Constants.BASE_URL).create(ApiInterface.class);
+        assertNotNull(messagesApi);
     }
 
     //Test to show if our endpoint call and config is correct
@@ -67,7 +79,7 @@ public class MainActivityTest {
     public void getsExpectedRequest() throws IOException, InterruptedException {
         try {
             mockBackend.enqueue(new MockResponse()
-                    .setBody(RestServiceTestHelper.getStringFromFile(getContext(), fileName))
+                    .setBody(RestServiceTestHelper.getStringFromFile(context, fileName))
             );
         } catch (Exception e) {
             e.printStackTrace();
@@ -86,7 +98,7 @@ public class MainActivityTest {
         try {
             mockBackend.enqueue(
                     new MockResponse()
-                            .setBody(RestServiceTestHelper.getStringFromFile(getContext(), fileName))
+                            .setBody(RestServiceTestHelper.getStringFromFile(context, fileName))
                             .setResponseCode(200)
                             .addHeader("Content-Type", "application/json;charset=utf-8")
             );
@@ -112,7 +124,7 @@ public class MainActivityTest {
         try {
             mockBackend.enqueue(
                     new MockResponse()
-                            .setBody(RestServiceTestHelper.getStringFromFile(getContext(), fileName))
+                            .setBody(RestServiceTestHelper.getStringFromFile(context, fileName))
                             .setResponseCode(500)
                             .addHeader("Content-Type", "application/json;charset=utf-8")
             );
@@ -135,7 +147,7 @@ public class MainActivityTest {
         String expectedResponse = "";
 
         try {
-            expectedResponse = RestServiceTestHelper.getStringFromFile(getContext(), fileName);
+            expectedResponse = RestServiceTestHelper.getStringFromFile(context, fileName);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -147,8 +159,8 @@ public class MainActivityTest {
         final Response<GuideResponse> response = mockApi().getData().execute();
         final GuideResponse guideResponse = response.body();
 
-        assertEquals(expectedResponse, new Gson().toJson(guideResponse));
-        //assertEquals("Many pages make a thick book.", guideResponse.guideList().get(0));
+        assertEquals("{\"total\":\"150\",\"data\":[{\"startDate\":\"Oct 05, 2019\",\"objType\":\"guide\",\"endDate\":\"Oct 05, 2019\",\"name\":\"Akron - Abernathy Construction Open House\",\"loginRequired\":false,\"url\":\"/guide/166682\",\"venue\":{},\"icon\":\"https://s3.amazonaws.com/media.guidebook.com/service/79PYfygE5bDED8KIjwHQVOX5OSX5AGFgoJ411qgO/logo.png\"},{\"startDate\":\"Oct 05, 2019\",\"objType\":\"guide\",\"endDate\":\"Oct 05, 2019\",\"name\":\"Belmont - Heathcliffe Construction Open House\",\"loginRequired\":false,\"url\":\"/guide/166613\",\"venue\":{},\"icon\":\"https://s3.amazonaws.com/media.guidebook.com/service/mYCUTUx1lHgYIJvKtbFxlqrYWpmHuKLRFQMDlepP/logo.png\"}]}",
+                new Gson().toJson(guideResponse));
     }
 
     @After
